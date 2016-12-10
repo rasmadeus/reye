75b31df9cf69f8dd3c46be66e0664787af6f4d0d
K. Kulikov <rasmadeus@gmail.com>
2016-12-10 19:39:08 +0300

    - Add track bar.

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,34 @@
+int slider_pos = 0;
+CvCapture* capture = nullptr;
+
+
+void onTrackbarSlide(int pos)
+{
+    cvSetCaptureProperty(capture, CV_CAP_PROP_POS_FRAMES, pos);
+}
+
 int main(int argc, char *argv[])
 {
-    auto img = cvLoadImage("res/rra.jpg");
-    cvNamedWindow("Display picture", CV_WINDOW_AUTOSIZE);
-    cvShowImage("Display picture", img);
-    cvWaitKey(0);
-    cvReleaseImage(&img);
-    cvDestroyWindow("Display picture");
+    cvNamedWindow("TrackBar", CV_WINDOW_AUTOSIZE);
+    capture = cvCreateFileCapture("res/rra.avi");
+
+    const auto frames = static_cast<int>(cvGetCaptureProperty(capture, CV_CAP_PROP_FRAME_COUNT));
+    if (0 != frames)
+        cvCreateTrackbar("Position", "TrackBar", &slider_pos, frames, onTrackbarSlide);
+
+    IplImage* frame;
+    while(true)
+    {
+        frame = cvQueryFrame(capture);
+        if (!frame)
+            break;
+
+        cvShowImage("TrackBar", frame);
+        const char c = cvWaitKey(33);
+        if (27 == c)
+            break;
+    }
+
+    cvReleaseCapture(&capture);
+    cvDestroyWindow("TrackBar");
 }
