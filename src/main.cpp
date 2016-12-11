10b2e58cec30aea45b956e01079d86427b14a076
K. Kulikov <rasmadeus@gmail.com>
2016-12-11 16:08:38 +0300

    - Add canny example.

--- src/main.cpp
+++ src/main.cpp
@@ -1,24 +1,28 @@
-void transform(IplImage* img)
+IplImage* doPyrDown(IplImage* in, int filter = IPL_GAUSSIAN_5x5)
 {
-    cvNamedWindow("in");
-    cvNamedWindow("out");
-    cvShowImage("in", img);
+    auto out = cvCreateImage(cvSize(in->width / 2, in->height / 2), in->depth, in->nChannels);
+    cvPyrDown(in, out, filter);
+    return out;
+}
 
-    auto out = cvCreateImage(cvGetSize(img), IPL_DEPTH_8U, 3);
-    cvSmooth(img, out, CV_BLUR_NO_SCALE, 3, 3);
-
-    cvShowImage("out", out);
-    cvReleaseImage(&out);
-
-    cvWaitKey(0);
-    cvDestroyWindow("out");
-    cvDestroyWindow("in");
+IplImage* doCanny(IplImage* in, double lowThresh, double highTresh, double aperture)
+{
+    auto out = cvCreateImage(cvGetSize(in), IPL_DEPTH_8U, 1);
+    cvCanny(in, out, lowThresh, highTresh, aperture);
+    return out;
 }
 
 
 int main(int argc, char *argv[])
 {
-    auto img = cvLoadImage("res/rra.jpg");
-    transform(img);
-    cvReleaseImage(&img);
+    cvNamedWindow("out");
+    auto in = cvLoadImage("res/rra.jpg");
+    auto out = doPyrDown(in);
+    out = doPyrDown(out);
+    out = doCanny(out, 10, 100, 3);
+    cvShowImage("out", out);
+    cvReleaseImage(&out);
+    cvReleaseImage(&in);
+    cvWaitKey(0);
+    cvDestroyWindow("out");
 }
