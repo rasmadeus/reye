fccc4a01f11036f4bf8e62d80f98d13676cd3147
K. Kulikov <rasmadeus@gmail.com>
2016-12-11 18:57:19 +0300

    - Capture from cam.

--- src/main.cpp
+++ src/main.cpp
@@ -1,28 +1,21 @@
-IplImage* doPyrDown(IplImage* in, int filter = IPL_GAUSSIAN_5x5)
-{
-    auto out = cvCreateImage(cvSize(in->width / 2, in->height / 2), in->depth, in->nChannels);
-    cvPyrDown(in, out, filter);
-    return out;
-}
-
-IplImage* doCanny(IplImage* in, double lowThresh, double highTresh, double aperture)
-{
-    auto out = cvCreateImage(cvGetSize(in), IPL_DEPTH_8U, 1);
-    cvCanny(in, out, lowThresh, highTresh, aperture);
-    return out;
-}
-
-
 int main(int argc, char *argv[])
 {
-    cvNamedWindow("out");
-    auto in = cvLoadImage("res/rra.jpg");
-    auto out = doPyrDown(in);
-    out = doPyrDown(out);
-    out = doCanny(out, 10, 100, 3);
-    cvShowImage("out", out);
-    cvReleaseImage(&out);
-    cvReleaseImage(&in);
-    cvWaitKey(0);
-    cvDestroyWindow("out");
+    auto capture = cvCreateCameraCapture(0);
+    if (capture)
+    {
+        IplImage* frame = nullptr;
+        cvNamedWindow("cam", CV_WINDOW_AUTOSIZE);
+        while(true)
+        {
+            frame = cvQueryFrame(capture);
+            if (!frame)
+                break;
+            cvShowImage("cam", frame);
+            char c = cvWaitKey(33);
+            if (27 == c)
+                break;
+        }
+        cvDestroyWindow("cam");
+    }
+    cvReleaseCapture(&capture);
 }
