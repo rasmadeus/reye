d8140a7b0eba2591f6a9a5a30b804d525928b3da
K. Kulikov <rasmadeus@gmail.com>
2016-12-17 17:38:24 +0300

    - Refactoring of window creating.

--- src/inc.h
+++ src/inc.h
@@ -8,29 +8,31 @@ void inc(const char* path_to_img, int x, int y, int width, int height, int add)
     cvAddS(img, cvScalar(add), img);
     cvResetImageROI(img);
 
-    cvNamedWindow("inc", 1);
+    cvNamedWindow("inc");
     cvShowImage("inc", img);
     cvReleaseImage(&img);
     cvWaitKey();
+    cvDestroyWindow("inc");
 }
 
 void blend(const char* first, const char* second, double alpha, double beta)
 {
-    auto img_first = cvLoadImage(first, 1);
-    auto img_second = cvLoadImage(second, 1);
+    auto img_first = cvLoadImage(first);
+    auto img_second = cvLoadImage(second);
 
     cvSetImageROI(img_first, cvRect(350, 460, img_second->width, img_second->height));
 
     cvAddWeighted(img_first, alpha, img_second, beta, 0.0, img_first);
     cvResetImageROI(img_first);
 
-    cvNamedWindow("img_first", 1);
+    cvNamedWindow("img_first");
     cvShowImage("img_first", img_first);
 
     cvReleaseImage(&img_second);
     cvReleaseImage(&img_first);
 
     cvWaitKey();
+    cvDestroyWindow("img_first");
 }
 
 
